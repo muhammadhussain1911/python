3123a9525d2b6ea954fb05fad15666f035031aed
Muhammad Hussain <ayesq0003@gmail.com>
2026-06-27 14:12:19 +0500

    Built a RPG character - freecodecamp python module learning

---
+++ python_free_code_camp/RPG_character.py
@@ -0,0 +1,27 @@
+def create_character(character_name, strength, intelligence, charisma):
+  full_dot = '●'
+  empty_dot = '○'
+  
+  if not isinstance(character_name, str):
+    return 'The character name should be a string'
+  if character_name == '':
+    return 'The character should have a name'
+  if len(character_name) > 10:
+    return 'The character name is too long'
+  if ' ' in character_name:
+    return 'The character name should not contain spaces'
+  if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
+    return 'All stats should be integers'
+  stat_sum = strength + intelligence + charisma
+  strength_diff = 10 - strength
+  intelligence_diff = 10 - intelligence
+  charisma_diff = 10 - charisma
+  if strength < 1 or intelligence < 1 or charisma < 1:
+    return 'All stats should be no less than 1'
+  if strength > 4 or intelligence > 4 or charisma > 4:
+    return 'All stats should be no more than 4'
+  if stat_sum != 7:
+    return 'The character should start with 7 points'
+  return f'{character_name}\nSTR {full_dot*strength}{empty_dot*strength_diff}\nINT {full_dot*intelligence}{empty_dot*intelligence_diff}\nCHA {full_dot*charisma}{empty_dot*charisma_diff}'
+
+create_character('ren', 4, '2', 1)
