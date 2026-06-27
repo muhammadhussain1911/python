9056576de1568c79df1281e33c307114d2c5b0e2
Muhammad Hussain <ayesq0003@gmail.com>
2026-06-27 08:45:14 +0500

    Refactor internal_working_of_python.py to remove unnecessary semicolons and improve readability.

--- internal_working_of_python.py
+++ internal_working_of_python.py
@@ -1,32 +1,32 @@
 # reference in numbers and strings.
 
 n1 = 24
-n2 = n1;
+n2 = n1
 
-n1 = "Hussain";
+n1 = "Hussain"
 
-print(n1);
-print(n2);
+print(n1)
+print(n2)
 
 # reference in list
 
 
-l1 = [1, 2, 3];
+l1 = [1, 2, 3]
 
-l2 = l1;
+l2 = l1
 
-l1[0] = 33;
+l1[0] = 33
 
 print(l1)
-print(l2);
+print(l2)
 
-k1 = [1, 2, 3];
+k1 = [1, 2, 3]
 
-k2 = [1, 2, 3];
+k2 = [1, 2, 3]
 
-k1[0] = 33;
+k1[0] = 33
 
 print(k1)
-print(k2);
+print(k2)
 
 
